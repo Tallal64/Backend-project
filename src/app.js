d215f3e7dc738a86fd97c655aa3bb4acaf0004a3
Tallal64 <firefoxtallal@gmail.com>
2025-01-21 10:14:17 +0500

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
-import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.json({ limit: "16kb" }))
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// importing routes
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users", userRouter);
 
 export default app;
